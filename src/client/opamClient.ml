ocaml/opam

commit e4fb4d0eeef08f8c4e2b53468155a8cc6c13e3a6

Continue to split opamClient.ml into pieces.
Now there is opamSolution.ml which deals with interfacing with asking the solver and applying the results.

src/client/opamState.ml
@@ -508,6 +508,164 @@ let print_env_warning ?(add_profile = false) t =
         opam_root
         add_profile
 
+let base_packages = List.map OpamPackage.Name.of_string [ "base-unix"; "base-bigarray"; "base-threads" ]
+
+let get_compiler_packages t comp =
+  let comp = compiler t comp in
+  let available = OpamPackage.to_map (Lazy.force t.available_packages) in
+
+  let pkg_available, pkg_not =
+    List.partition
+      (fun (n, _) -> OpamPackage.Name.Map.mem n available)
+      (OpamFormula.atoms (OpamFile.Comp.packages comp)) in
+
+  (* check that all packages in [comp] are in [available] except for
+     "base-..."  (depending if "-no-base-packages" is set or not) *)
+  let pkg_not = List.rev_map (function (n, _) -> n) pkg_not in
+  let pkg_not =
+    if !OpamGlobals.base_packages then
+      pkg_not
+    else
+      List.filter (fun n -> not (List.mem n base_packages)) pkg_not in
+  if pkg_not <> [] then (
+    List.iter (OpamPackage.Name.to_string |> OpamGlobals.error "Package %s not found") pkg_not;
+    OpamGlobals.exit 2
+  );
+
+  pkg_available
+
+let add_switch root switch compiler =
+  log "add_switch switch=%s compiler=%s" (OpamSwitch.to_string switch) (OpamCompiler.to_string compiler);
+  let aliases_f = OpamPath.aliases root in
+  let aliases = OpamFile.Aliases.safe_read aliases_f in
+  if not (OpamSwitch.Map.mem switch aliases) then begin
+    OpamFile.Aliases.write aliases_f (OpamSwitch.Map.add switch compiler aliases);
+  end
+
+(* install ~/.opam/<switch>/config/conf-ocaml.config *)
+let install_conf_ocaml_config root switch =
+  log "install_conf_ocaml_config switch=%s" (OpamSwitch.to_string switch);
+  (* .config *)
+  let vars =
+    let map f l = List.map (fun (s,p) -> OpamVariable.of_string s, S (f p)) l in
+    let id x = x in
+
+    map OpamFilename.Dir.to_string
+      [
+        ("prefix", OpamPath.Switch.root root switch);
+        ("lib", OpamPath.Switch.lib_dir root switch);
+        ("bin", OpamPath.Switch.bin root switch);
+        ("doc", OpamPath.Switch.doc_dir root switch);
+        ("stublibs", OpamPath.Switch.stublibs root switch);
+        ("toplevel", OpamPath.Switch.toplevel root switch);
+        ("man", OpamPath.Switch.man_dir root switch);
+      ]
+    @ map id [
+      ("user" , try (Unix.getpwuid (Unix.getuid ())).Unix.pw_name with _ -> "user");
+      ("group", try (Unix.getgrgid (Unix.getgid ())).Unix.gr_name with _ -> "group");
+      ("make" , Lazy.force !OpamGlobals.makecmd);
+      ("os"   , Lazy.force OpamGlobals.os_string);
+    ] in
+
+  let config = OpamFile.Dot_config.create vars in
+  OpamFile.Dot_config.write (OpamPath.Switch.config root switch OpamPackage.Name.default) config
+
+(* - compiles and install $opam/compiler/[ocaml_version].comp in $opam/[switch]
+   - update $opam/switch
+   - update $opam/config *)
+let install_compiler t ~quiet switch compiler =
+  log "install_compiler switch=%s compiler=%s"
+    (OpamSwitch.to_string switch)
+    (OpamCompiler.to_string compiler);
+
+  let comp_f = OpamPath.compiler t.root compiler in
+  if not (OpamFilename.exists comp_f) then (
+    OpamGlobals.msg "Cannot find %s: %s is not a valid compiler name.\n"
+      (OpamFilename.to_string comp_f)
+      (OpamCompiler.to_string compiler);
+    OpamGlobals.exit 0;
+  );
+
+  let switch_dir = OpamPath.Switch.root t.root switch in
+  if OpamFilename.exists_dir switch_dir then (
+    OpamGlobals.msg "The compiler %s is already installed.\n" (OpamSwitch.to_string switch);
+    OpamGlobals.exit 0;
+  );
+
+  (* Create base directories *)
+  OpamFilename.mkdir switch_dir;
+  OpamFilename.mkdir (OpamPath.Switch.lib_dir t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.stublibs t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.toplevel t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.build_dir t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.bin t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.doc_dir t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.man_dir t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.install_dir t.root switch);
+  OpamFilename.mkdir (OpamPath.Switch.config_dir t.root switch);
+  List.iter (fun num ->
+    OpamFilename.mkdir (OpamPath.Switch.man_dir ~num t.root switch)
+  ) ["1";"1M";"2";"3";"4";"5";"6";"7";"9"];
+
+  install_conf_ocaml_config t.root switch;
+
+  let comp = OpamFile.Comp.read comp_f in
+  begin try
+    if not (OpamFile.Comp.preinstalled comp) then begin
+
+      OpamGlobals.verbose := not quiet;
+
+      (* Install the compiler *)
+      let comp_src = match OpamFile.Comp.src comp with
+        | Some f -> f
+        | None   ->
+          OpamGlobals.error_and_exit
+            "No source for compiler %s"
+            (OpamCompiler.to_string compiler) in
+      let build_dir = OpamPath.Switch.build_ocaml t.root switch in
+      let comp_src_raw = OpamFilename.to_string comp_src in
+      if Sys.file_exists comp_src_raw && Sys.is_directory comp_src_raw then
+        OpamFilename.link_dir (OpamFilename.Dir.of_string comp_src_raw) build_dir
+      else if Sys.file_exists comp_src_raw then
+        OpamFilename.extract comp_src build_dir
+      else OpamFilename.with_tmp_dir (fun download_dir ->
+        let file = OpamFilename.download comp_src download_dir in
+        OpamFilename.extract file build_dir;
+      );
+      let patches = OpamFile.Comp.patches comp in
+      let patches = List.map (fun f -> OpamFilename.download f build_dir) patches in
+      List.iter (fun f -> OpamFilename.patch f build_dir) patches;
+      if OpamFile.Comp.configure comp @ OpamFile.Comp.make comp <> [] then begin
+        OpamFilename.exec build_dir
+          [ ( "./configure" :: OpamFile.Comp.configure comp )
+            @ [ "-prefix";  OpamFilename.Dir.to_string switch_dir ]
+          (*-bindir %s/bin -libdir %s/lib -mandir %s/man*)
+          (* NOTE In case it exists 2 '-prefix', in general the script
+             ./configure will only consider the last one, others will be
+             discarded. *)
+          ; ( Lazy.force !OpamGlobals.makecmd :: OpamFile.Comp.make comp )
+          ; [ Lazy.force !OpamGlobals.makecmd ; "install" ]
+          ]
+      end else begin
+        let t = { t with switch } in
+        let builds =
+          List.map (List.map (substitute_string t)) (OpamFile.Comp.build comp) in
+        OpamFilename.exec build_dir builds
+      end;
+    end;
+
+    (* write the new version in the configuration file *)
+    let config = OpamFile.Config.with_switch t.config switch in
+    OpamFile.Config.write (OpamPath.config t.root) config;
+    add_switch t.root switch compiler
+
+  with e ->
+    if not !OpamGlobals.debug then
+      OpamFilename.rmdir switch_dir;
+    raise e
+  end
+
+
 module Types = struct
   type t = state = {
     root: OpamPath.t;

src/client/opamState.mli
@@ -125,6 +125,12 @@ val string_of_repositories: OpamFile.Repo_config.t repository_name_map -> string
 (** Return the list of available compilers *)
 val compilers: state -> compiler_set
 
+(** Install the given compiler *)
+val install_compiler: state -> quiet:bool -> switch -> compiler -> unit
+
+(** Get the packages associated with the given compiler *)
+val get_compiler_packages: state -> compiler -> atom list
+
 (** {2 Packages} *)
 
 (** Check whether a package name is installed *)
@@ -146,6 +152,9 @@ val find_packages_by_name: state -> name -> package_set option
 
 (** Return a map from package names to package installed version *)
 val installed_map: state -> version name_map
+
+(** Return the base packages *)
+val base_packages: name list
 
 (** {2 Configuration files} *)
 

src/client/client.ocp
@@ -4,7 +4,9 @@ begin library "opam-lib"
 
   files   = [
     "opamState.ml"
-    "opamConfig.ml"
+    "opamSolution.ml"
+    "opamConfigCommand.ml"
+    "opamSwitchCommand.ml"
     "opamClient.ml"
   ]
 

src/client/opamSolution.mli
@@ -1,0 +1,69 @@
+(***********************************************************************)
+(*                                                                     *)
+(*    Copyright 2012 OCamlPro                                          *)
+(*    Copyright 2012 INRIA                                             *)
+(*                                                                     *)
+(*  All rights reserved.  This file is distributed under the terms of  *)
+(*  the GNU Public License version 3.0.                                *)
+(*                                                                     *)
+(*  OPAM is distributed in the hope that it will be useful,            *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of     *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *)
+(*  GNU General Public License for more details.                       *)
+(*                                                                     *)
+(***********************************************************************)
+
+(** Applying solver solutions *)
+
+open OpamTypes
+
+(** Resolve an user request *)
+val resolve:
+  OpamState.state ->
+  user_action ->
+  atom request ->
+  (solution, string) result
+
+(** Resolve a request and apply the solution *)
+val resolve_and_apply:
+  ?force:bool ->
+  OpamState.state ->
+  user_action ->
+  atom request ->
+  solver_result
+
+(** Raise an error if no solution is found *)
+val error_if_no_solution: solver_result -> unit
+
+(** Raise an error because a package is unknown *)
+val unknown_package: name -> version option -> 'a
+
+(** Raise an error because a package in unavailable *)
+val unavailable_package: name -> version option -> 'a
+
+(** {2 Atoms} *)
+
+(** Return an atom with a string version constraint *)
+val eq_atom: name -> version -> atom
+
+(** Return a simple atom, with no version constrain, from a package*)
+val atom_of_package: package -> atom
+
+(** Return a list of simple atoms (ie. with no version constraints)
+    from a set of packages *)
+val atoms_of_packages: package_set -> atom list
+
+(** Return a list of atoms from a list of names (wich can eventually
+    be of the form name.version) *)
+val atoms_of_names: OpamState.state -> name_set -> atom list
+
+(** {2 Stats} *)
+val sum: stats -> int
+
+(** {2 Actions} *)
+
+(** Apply a solution return by the solver *)
+val apply_solution: ?force:bool -> OpamState.state -> solution -> solver_result
+
+(** Remove a package *)
+val proceed_to_delete: rm_build:bool -> OpamState.state -> package -> unit

git-inria/opam-backup

commit 53b8e86812d9559968e1aa3cfa48fe3bdad16c8f

Fix install of pinned packages
This commit fixes a regression introduced by e4fb4d0eeef08f8c4e2b53468155a8cc6c13e3a6

src/client/opamClient.ml
@@ -232,16 +232,6 @@ let update_repositories t ~show_compilers repositories =
     ) comps
   ) (sorted_repositories t)
 
-let update_pinned_package t nv pin =
-  let kind = kind_of_pin_option pin in
-  let path = OpamFilename.raw_dir (path_of_pin_option pin) in
-  let module B = (val OpamRepository.find_backend kind: OpamRepository.BACKEND) in
-  let build = OpamPath.Switch.build t.root t.switch nv in
-  match B.download_dir nv ~dst:build path with
-  | Up_to_date _    -> None
-  | Result _
-  | Not_available   -> Some nv
-
 (* Update the package contents, display the new packages and update reinstall *)
 let update_packages t ~show_packages repositories =
   log "update_packages %s" (OpamState.string_of_repositories repositories);
@@ -250,11 +240,16 @@ let update_packages t ~show_packages repositories =
     OpamPackage.Set.of_list (
       OpamMisc.filter_map
         (function
-          | n, (Path p | Git p as k) ->
+          | n, (Path p | Git p as pin) ->
             if OpamState.mem_installed_package_by_name t n then
               let nv = OpamState.find_installed_package_by_name t n in
               OpamGlobals.msg "Synchronizing with %s\n" (OpamFilename.Dir.to_string p);
-              update_pinned_package t nv k
+              match OpamState.update_pinned_package t nv pin with
+              | Up_to_date _  -> None
+              | Result _      -> Some nv
+              | Not_available ->
+                OpamGlobals.error "%s is not available" (OpamFilename.Dir.to_string p);
+                None
             else
               None
           | _ -> None)

src/client/opamSolution.ml
@@ -162,6 +162,7 @@ let pinned_path t nv =
     None
 
 let get_archive t nv =
+  log "get_archive %s" (OpamPackage.to_string nv);
   let aux repo_p repo =
     OpamRepository.download repo nv;
     let src = OpamPath.Repository.archive repo_p nv in
@@ -177,9 +178,14 @@ let extract_package t nv =
   log "extract_package: %s" (OpamPackage.to_string nv);
   let p_build = OpamPath.Switch.build t.root t.switch nv in
   match pinned_path t nv with
-  | Some (Git p| Path p) ->
+  | Some (Git p| Path p as pin) ->
     OpamGlobals.msg "Synchronizing pinned package\n";
-    OpamFilename.mkdir p_build;
+    if not (OpamFilename.exists_dir p_build) then (
+      match OpamState.update_pinned_package t nv pin with
+      | Not_available -> OpamGlobals.error "%s is not available" (OpamFilename.Dir.to_string p)
+      | Result _
+      | Up_to_date _  -> ()
+    );
     let _files = OpamState.with_repository t nv (fun repo _ ->
       OpamFilename.in_dir p_build (fun () -> OpamRepository.copy_files repo nv)
     ) in

src/client/opamState.ml
@@ -686,6 +686,12 @@ let install_compiler t ~quiet switch compiler =
     raise e
   end
 
+let update_pinned_package t nv pin =
+  let kind = kind_of_pin_option pin in
+  let path = OpamFilename.raw_dir (path_of_pin_option pin) in
+  let module B = (val OpamRepository.find_backend kind: OpamRepository.BACKEND) in
+  let build = OpamPath.Switch.build t.root t.switch nv in
+  B.download_dir nv ~dst:build path
 
 module Types = struct
   type t = state = {

src/client/opamState.mli
@@ -172,6 +172,11 @@ val compiler: state -> compiler -> OpamFile.Comp.t
 (** Apply a function while taking the right locks *)
 val check: lock -> unit
 
+(** {2 Pinned packages} *)
+
+(** Update pinned package *)
+val update_pinned_package: state -> package -> pin_option -> dirname download
+
 (** To be able to open [OpamState.Types] *)
 module Types: sig
   type t = state = {